60087c5faaaccd23d7ce9789aad64460e98ec98f
UMESH-ARADHYA-P <hphp9481@gmail.com>
2023-01-14 13:28:56 +0530

    commented

--- test/test1.py
+++ test/test1.py
@@ -1,8 +1,8 @@
 
-from flask import Flask  ,redirect
-app = Flask(__name__) 
-@app.route('/')
-def index():
-    return redirect('https://www.w3schools.com/html/default.asp')
-if __name__ =="__main__":  
-    app.run(debug = True)
+# from flask import Flask  ,redirect
+# app = Flask(__name__) 
+# @app.route('/')
+# def index():
+#     return redirect('https://www.w3schools.com/html/default.asp')
+# if __name__ =="__main__":  
+#     app.run(debug = True)
