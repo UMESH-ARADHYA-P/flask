e8a495b4bbd3def2fa0c98e358dc44f8df46f0d9
UMESH-ARADHYA-P <hphp9481@gmail.com>
2023-01-15 08:33:41 +0530

    references is added

--- part 1/chapter 3/error_handling.py
+++ part 1/chapter 3/error_handling.py
@@ -5,7 +5,7 @@ from flask import abort
 app = Flask(__name__)
 @app.route('/user/<id>')
 def get_user(id):
-    user = load_user(id)
+    user =1 #load_user(id)
     if not user:
         abort(404)
     return '<h1>Hello, %s</h1>' % user.name
